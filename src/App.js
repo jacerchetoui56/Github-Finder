72af26d53a78f29263bf878b4ecfc39b0e096364
jacerchetoui56 <77737324+jacerchetoui56@users.noreply.github.com>
2022-07-12 13:51:52 +0100

    adding the main files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="m-4 mx-auto max-w-xl">
+      <header className="flex justify-between my-6 items-center">
+        <h1 className="text-white font-mono text-2xl font-bold">GitFinder</h1>
+        <div >
+          <span className="uppercase tracking-wide font-medium text-white text-base">Light</span>
+          {/* icons (dark and light) */}
+        </div>
       </header>
+      <main>
+        <div className="flex bg-primary-100 justify-between p-2 rounded-lg">
+          {/* search icon */}
+          <input
+            className="text-slate-200 bg-transparent border-none text-lg p-2 font-medium 
+            tracking-wider font-mono flex-1 focus:outline-none"
+            type="text"
+            placeholder="Search Github username" />
+          <button className="rounded-lg font-medium px-4 py-2 text-sm bg-blue-600 text-white tracking-wide text-center">Search</button>
+        </div>
+
+
+      </main>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: 'rgb(31,42,72)',
+          200: 'rgb(20,28,47)'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
